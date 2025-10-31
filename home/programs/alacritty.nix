1e7609d014d417138880fac1f428739498c9a644
Kha Mui <khamui@me.com>
2025-10-31 17:27:39 +0100

    ala

--- home/programs/alacritty.nix
+++ home/programs/alacritty.nix
@@ -1,12 +1,6 @@
 {pkgs, ...}: {
   programs.alacritty = {
     enable = true;
-    settings = {
-      shell = {
-        program = "${pkgs.zsh}/bin/zsh";
-        args = ["-l"];
-      };
-    };
   };
   home.file.".config/alacritty/alacritty.toml".source = ./dotfiles/.alacritty.toml;
 }

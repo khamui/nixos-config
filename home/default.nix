eccd39cb27912f900c947e2fb79bc5b20c6ed9d2
Kha Mui <khamui@me.com>
2025-10-31 20:28:20 +0100

    ala

--- home/default.nix
+++ home/default.nix
@@ -96,11 +96,6 @@
     #pciutils # lspci
     #usbutils # lsusb
   ];
-
-  fonts.fonts = with pkgs; [
-    jetbrains-mono
-  ];
-
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new home Manager release introduces backwards

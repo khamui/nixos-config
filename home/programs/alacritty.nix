c3dd98825b25b4524e23c6b83a9c4d1640c60794
Kha Mui <khamui@me.com>
2025-10-31 15:12:31 +0100

    lds

--- home/programs/alacritty.nix
+++ home/programs/alacritty.nix
@@ -3,15 +3,16 @@
   programs.alacritty = {
     enable = true;
     # custom settings
-    settings = {
-      env.TERM = "xterm-256color";
-      font = {
-        size = 12;
-        draw_bold_text_with_bright_colors = true;
-      };
-      scrolling.multiplier = 5;
-      selection.save_to_clipboard = true;
-      general.import = [pkgs.alacritty-theme.github_light];
-    };
+    initExtra = builtins.readFile ./dotfiles/.alacritty.toml;
+    #settings = {
+    #  env.TERM = "xterm-256color";
+    #  font = {
+    #    size = 12;
+    #    draw_bold_text_with_bright_colors = true;
+    #  };
+    #  scrolling.multiplier = 5;
+    #  selection.save_to_clipboard = true;
+    #  general.import = [pkgs.alacritty-theme.github_light];
+    #};
   };
 }
